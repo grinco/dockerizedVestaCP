9631cdd5608c939a87057beabbff97b5aef6e555
Vadim Grinco <vgrinco@redhat.com>
2017-02-12 22:07:19 +0100

    It looks really wrong.
    Signed-off-by: Vadim Grinco <vgrinco@redhat.com>

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -504,9 +504,7 @@ fi
 
 # Deleting old admin user
 if [ ! -z "$(grep ^admin: /etc/passwd)" ] && [ "$force" = 'yes' ]; then
-    #chattr -i /home/admin/conf > /dev/null 2>&1
     userdel -f admin >/dev/null 2>&1
-    #chattr -i /home/admin/conf >/dev/null 2>&1
     rm -f /tmp/sess_* >/dev/null 2>&1
 fi
 if [ ! -z "$(grep ^admin: /etc/group)" ] && [ "$force" = 'yes' ]; then
